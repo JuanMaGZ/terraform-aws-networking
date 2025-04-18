27347f87e44084e241b10b1d22e1de1cc86e0d43
juanmaps03 <juan.guzman@connectupweb.com>
2025-04-18 15:14:32 -0500

    Update variables descriiptions

--- variables.tf
+++ variables.tf
@@ -1,4 +1,7 @@
 variable "vpc_config" {
+
+  description = "Contains the configuration for the VPC. More especially, the CIDR block and the name of the VPC."
+
   type = object({
     cidr_block = string
     name       = string
@@ -11,6 +14,15 @@ variable "vpc_config" {
 }
 
 variable "subnet_config" {
+
+  description = <<EOF
+  Accepts a map of objects that define the configuration for the subnets. Each object must contain the following attributes:
+  - cidr_block: The CIDR block for the subnet.
+  - public: A boolean value that indicates whether the subnet is public or private. Defaults to false.
+  - az: The availability zone in which the subnet will be created. This is required.
+  The map key is used to identify the subnet and must be unique.
+  EOF
+
   type = map(object({
     cidr_block = string
     public     = optional(bool, false)
